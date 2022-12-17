b004b5216359532f7b9e010f607ce9631f2725f5
Craig Treasure <craigktreasure@outlook.com>
2022-12-16 22:58:51 -0800

    Allow latest SDK again and patch on macOS (#160)
    This change reverts a portion of my last change to allow us to use the
    latest .NET 7 SDK again. The issue is on macOS where `dotnet format` on
    7.0.101 is broken, so i've included a script that patches global.json on
    macOS to disable the `rollForward` option. I've also installed 2
    versions of the .NET 7 SDK on build agents to allow both variations to
    work.

---
+++ scripts/PatchGlobalJson.ps1
@@ -0,0 +1,9 @@
+if (-not $IsMacOS) {
+    return
+}
+
+Write-Host 'Disabling rollForward to pin to version in global.json.'
+$globalJsonPath = Join-Path $PSScriptRoot '../global.json'
+$globalJson = Get-Content $globalJsonPath -Raw | ConvertFrom-Json
+$globalJson.sdk.rollForward = 'disable'
+$globalJson | ConvertTo-Json | Set-Content -Path $globalJsonPath
