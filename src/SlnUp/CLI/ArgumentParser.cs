1513ca558faac3a754e8553621cedb9e84334929
Craig Treasure <craigktreasure@outlook.com>
2023-09-05 20:20:59 -0700

    Address the new CA1863 warning in .NET 8

--- src/SlnUp/CLI/ArgumentParser.cs
+++ src/SlnUp/CLI/ArgumentParser.cs
@@ -7,6 +7,10 @@ internal static class ArgumentParser
 {
     private const string CannotParseArgumentOption = "Cannot parse argument '{0}' for option '{1}' as expected type '{2}'.";
 
+#if NET8_0_OR_GREATER
+    private static readonly System.Text.CompositeFormat CannotParseArgumentOptionFormat = System.Text.CompositeFormat.Parse(CannotParseArgumentOption);
+#endif
+
     public static Version? ParseVersion(ArgumentResult result)
     {
         string tokenValue = result.Tokens.Single().Value;
@@ -17,7 +21,11 @@ internal static class ArgumentParser
 
         result.ErrorMessage = string.Format(
             CultureInfo.InvariantCulture,
+#if NET8_0_OR_GREATER
+            CannotParseArgumentOptionFormat,
+#else
             CannotParseArgumentOption,
+#endif
             tokenValue,
             result.Argument.Name,
             result.Argument.ValueType);
